f10cdddc936bfd88df4418097cca99e0246c213a
n284 <n_kyo56@icloud.com>
2023-01-15 22:19:19 +0900

    Upload project.

---
+++ library-service/infractructure/src/main/java/com/example/infractructure/InfractructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.infractructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfractructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfractructureApplication.class, args);
+	}
+
+}
---
+++ library-service/infractructure/src/test/java/com/example/infractructure/InfractructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infractructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
